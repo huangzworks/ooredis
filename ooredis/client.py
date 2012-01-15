c2befdb87c7e09e8b05316a3531bd3e65d412da6
huangz1990 <huangz1990@gmail.com>
2012-01-15 20:21:34 +0800

    移除 client.py 中无用的私有化

--- ooredis/client.py
+++ ooredis/client.py
@@ -2,22 +2,20 @@
 
 __all__ = ['connect', 'get_client']
 
-__metaclass__ = type
-
 import redis
 
-__client = None
+client = None
 
 def connect(*args, **kwargs):
     """ 连接Redis数据库，参数和redis-py的Redis类一样 """
-    global __client
-    __client = redis.Redis(*args, **kwargs)
+    global client
+    client = redis.Redis(*args, **kwargs)
 
 def get_client():
     """ 返回OORedis客户端 """
-    global __client
+    global client
 
-    if __client == None:
-        __client = redis.Redis()
+    if client == None:
+        client = redis.Redis()
 
-    return __client
+    return client
