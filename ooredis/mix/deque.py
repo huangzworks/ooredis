90589d2a2dca014d006349deb4915ceafa5c9eda
huangz1990 <huangz1990@gmail.com>
2012-06-14 03:39:28 +0800

    使用 catch_wrong_type_error 重构 Deque 类

--- ooredis/mix/deque.py
+++ ooredis/mix/deque.py
@@ -8,10 +8,13 @@ import collections
 import redis.exceptions as redispy_exception
 
 from ooredis.mix.key import Key
-from ooredis.mix.helper import format_key
+from ooredis.mix.helper import format_key, catch_wrong_type_error
 
 class Deque(Key):
-    """ 一个双端队列 key 对象，底层实现是 redis 的 list 类型。 """
+
+    """ 
+    一个双端队列 key 对象，底层实现是 redis 的 list 类型。 
+    """
 
     def append(self, python_item):
         """
@@ -29,12 +32,11 @@ class Deque(Key):
         Raises:
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            redis_item = self._type_case.to_redis(python_item)
-            self._client.rpush(self.name, redis_item)
-        except redispy_exception.ResponseError:
-            raise TypeError
+        redis_item = self._type_case.to_redis(python_item)
+        self._client.rpush(self.name, redis_item)
 
+
+    @catch_wrong_type_error
     def appendleft(self, python_item):
         """
         将元素 item 追加到队列的最左边。
@@ -51,11 +53,9 @@ class Deque(Key):
         Raises:
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            redis_item = self._type_case.to_redis(python_item)
-            self._client.lpush(self.name, redis_item)
-        except redispy_exception.ResponseError:
-            raise TypeError
+        redis_item = self._type_case.to_redis(python_item)
+        self._client.lpush(self.name, redis_item)
+
 
     def extend(self, python_iterable):
         """
@@ -73,12 +73,11 @@ class Deque(Key):
         Raises:
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            redis_iterable = map(self._type_case.to_redis, python_iterable)
-            self._client.rpush(self.name, *redis_iterable)
-        except redispy_exception.ResponseError:
-            raise TypeError
+        redis_iterable = map(self._type_case.to_redis, python_iterable)
+        self._client.rpush(self.name, *redis_iterable)
 
+
+    @catch_wrong_type_error
     def extendleft(self, python_iterable):
         """
         将 iterable 内的所有元素追加到队列的最左边。
@@ -100,11 +99,9 @@ class Deque(Key):
         Raises:
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            redis_iterable = map(self._type_case.to_redis, python_iterable)
-            self._client.lpush(self.name, *redis_iterable)
-        except redispy_exception.ResponseError:
-            raise TypeError
+        redis_iterable = map(self._type_case.to_redis, python_iterable)
+        self._client.lpush(self.name, *redis_iterable)
+
 
     def clear(self):
         """
@@ -121,6 +118,7 @@ class Deque(Key):
         """
         self.delete()
     
+
     def count(self, python_item):
         """
         计算队列中和 item 相等的元素的个数。
@@ -140,6 +138,8 @@ class Deque(Key):
         all_python_item = list(self)
         return all_python_item.count(python_item)
 
+
+    @catch_wrong_type_error
     def pop(self):
         """
         移除并返回队列最右边的元素。
@@ -159,16 +159,15 @@ class Deque(Key):
             IndexError: 尝试对空队列执行 pop 操作时抛出。
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            redis_item = self._client.rpop(self.name)
-            python_item = self._type_case.to_python(redis_item)
-            if python_item is None:
-                raise IndexError
-            else:
-                return python_item
-        except redispy_exception.ResponseError:
-            raise TypeError
+        redis_item = self._client.rpop(self.name)
+        python_item = self._type_case.to_python(redis_item)
+        if python_item is None:
+            raise IndexError
+        else:
+            return python_item
 
+
+    @catch_wrong_type_error
     def popleft(self):
         """
         移除并返回队列最左边的元素。
@@ -188,19 +187,19 @@ class Deque(Key):
             IndexError: 尝试对空队列执行 pop 操作时抛出。
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            redis_item = self._client.lpop(self.name)
-            python_item = self._type_case.to_python(redis_item)
-            if python_item is None:
-                raise IndexError
-            else:
-                return python_item
-        except redispy_exception.ResponseError:
-            raise TypeError
+        redis_item = self._client.lpop(self.name)
+        python_item = self._type_case.to_python(redis_item)
+        if python_item is None:
+            raise IndexError
+        else:
+            return python_item
+
 
     def __repr__(self):
         return format_key(self, self.name, list(self))
 
+
+    @catch_wrong_type_error
     def __len__(self):
         """
         返回队列中的元素个数。
@@ -215,10 +214,8 @@ class Deque(Key):
         Raises:
             TypeError: 尝试对非 list 类型的 key 进行操作时抛出。
         """
-        try:
-            return self._client.llen(self.name)
-        except redispy_exception.ResponseError:
-            raise TypeError
+        return self._client.llen(self.name)
+
 
     def __iter__(self):
         """
@@ -241,12 +238,15 @@ class Deque(Key):
         except redispy_exception.ResponseError:
             raise TypeError
 
+
     def __delitem__(self, key):
         raise Exception 
+
 
     def __getitem__(self, index):
         all_item = list(self)
         return all_item[index]
+
 
     def __setitem__(self, index, item):
         raise Exception
