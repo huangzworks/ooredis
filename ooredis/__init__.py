cb204eb99f78e0440c96b68d71a1d241d3ebb8e0
huangz1990 <huangz1990@gmail.com>
2012-06-14 03:30:26 +0800

    移除 List 类

--- ooredis/__init__.py
+++ ooredis/__init__.py
@@ -3,7 +3,7 @@
 __all__ = [
     'connect', 'get_client',
     'type_case',
-    'Dict', 'List', 'Set', 'SortedSet', 'String', 'Counter', 'Deque',
+    'Dict', 'Set', 'SortedSet', 'String', 'Counter', 'Deque',
     '__version__',
 ]
 
@@ -12,7 +12,6 @@ import type_case
 from client import connect, get_client
 
 from mix.dict import Dict
-from mix.list import List
 from mix.set import Set
 from mix.sorted_set import SortedSet
 from mix.string import String
