8baccdab650a4b7bc7113c9e7d2420cb9328e0a3
huangz1990 <huangz1990@gmail.com>
2012-09-19 11:48:57 +0800

    升级版本至 1.9.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='ooredis',
-      version='1.9.1',
+      version='1.9.2',
       author='huangz',
       author_email='huangz1990@gmail.com',
       url='https://github.com/huangz1990/ooredis',
-      description='An redis python lib, see github.com/huangz1990/ooredis for more information.',
+      description='A redis-to-python mapper, see github.com/huangz1990/ooredis for more information.',
       long_description='',
       download_url='',
       packages=['ooredis', 'ooredis.key', 'ooredis.type_case',],
