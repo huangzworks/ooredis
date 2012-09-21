b08037da59253629742144278452cc173e7b6710
huangz1990 <huangz1990@gmail.com>
2012-09-21 10:17:16 +0800

    整理 ooredis.const 文档

--- ooredis/const.py
+++ ooredis/const.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# redis的数据类型
+# redis 的数据类型
 REDIS_TYPE = {
     'string': 'string',
     'list': 'list',
@@ -10,7 +10,7 @@ REDIS_TYPE = {
     'not_exists': 'none',
 }
 
-# redis列表和有序集的range边界
+# redis 列表和有序集的 range 边界
 LEFTMOST = 0
 RIGHTMOST = -1
 
