7e1cd22c6e594c6827405e06541625a0852b8b04
huangz1990 <huangz1990@gmail.com>
2012-09-22 15:24:52 +0800

    升级版本至 1.9.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ooredis',
-      version='1.9.5',
+      version='1.9.6',
       author='huangz',
       author_email='huangz1990@gmail.com',
       url='https://github.com/huangz1990/ooredis',
